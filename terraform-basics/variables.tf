71182e5776f3d04bbc8e052c90a5da360f1d19e7
Md. Zahid Hasan <zahid.s.dev@gmail.com>
2026-04-20 12:28:33 +0600

    variable md file added

--- terraform-basics/variables.tf
+++ terraform-basics/variables.tf
@@ -19,3 +19,12 @@ variable "Security-Group" {
   type = "list"
   default = ["sg-0a1b2c3d4e5f6g7h8i9j"]  
 }
+#map
+variable "AMIS" {
+  type = map(string)
+  default = {
+    "us-east-1" = "ami-0123456789abcdef0"
+    "us-west-2" = "ami-0fedcba9876543210"
+  }
+  
+}
